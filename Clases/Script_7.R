3220be1748a537cc3d2c97d956cd9d3eba7909a2
Marco Gzz <marco.gonzaleztg@uanl.edu.mx>
2022-09-27 10:37:28 -0500

    Update

--- Clases/Script_7.R
+++ Clases/Script_7.R
@@ -30,6 +30,55 @@ fligner.test(avena$prod, avena$suelo)
 
 # Revisar datos de forma gráfica
 
+boxplot(avena$prod ~ avena$suelo)
+
+# Suma de cuadrados total
+SST <- sum((avena$prod - mean(avena$prod))^2)
+
+# Suma de cuadrados del error
+arena - mean(arena)
+arcilla-mean(arcilla)
+limo-mean(limo)
+
+area.sum <- sum((arena - mean(arena))^2)
+arcilla.sum <- sum((arcilla-mean(arcilla))^2)
+limo.sum <- sum((limo-mean(limo))^2)
+
+SSe <- area.sum + arcilla.sum + limo.sum
+SSe
+
+SStr <- SST - SSe
+SStr
 
 
+# Cuadrado medio 
+tapply(avena$prod, avena$suelo, var)
+CME <- mean(tapply(avena$prod, avena$suelo, var))
+CMtr <- SStr/2
+Fcal <- CMtr/CME
+Fcal
+
+Ftab <- qf(0.95,2,27)
+Ftab
+
+
+probF <- 1 - pf(Fcal, 2, 27)
+probF
+
+
+# ANOVA procedimiento simplificado 
+
+
+avena.aov <- aov(avena$prod ~ avena$suelo)
+summary(avena.aov)
+
+par(mfrow=c(2,2))
+plot(aov(avena$prod ~ avena$suelo))
+par(mfrow=c(1,1))
+
+
+TukeyHSD(avena.aov, conf.level = 0.95)
+
+
+plot(TukeyHSD(avena.aov))
 
